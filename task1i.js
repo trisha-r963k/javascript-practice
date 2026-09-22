f462d57b4151a77d17543d9ec9ac4a8ed5dbfd3e
Sanchita Dey <deysanchita8822@gmail.com>
2026-09-22 16:47:19 +0530

    Add Task 1

---
+++ task1i.js
@@ -0,0 +1,15 @@
+let student = {
+    name : "Radha",
+    age : 16 ,
+    course : "Javascript",
+    college : "SITM" ,
+    marks : 100 ,
+    birthyear : 2006
+}
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.course);
+console.log(student.college);
+console.log(student.marks);
+console.log(student.birthyear);
